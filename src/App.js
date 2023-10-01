c5e09f63ab3f68bccfc07f9bb711c8c6921c4c14
bondyaroslav <bondy2526@gmail.com>
2023-10-01 15:24:47 +0300

    save, add layout

--- src/App.js
+++ src/App.js
@@ -7,48 +7,13 @@ import Calendar from "./Components/Calendar/Calendar";
 const App = ({currentYear, currentDate, children}) => {
     console.log(currentYear)
 
-    const [pages, setPages] = useState([])
-    const [offset, setOffset] = useState(0)
-
-    useEffect(() => {
-        setPages(
-            Children.map(children, child => {
-                return cloneElement(child, {
-                    style: {
-                        height: "100%",
-                        minWidth: "100%",
-                        maxWidth: "100%"
-                    }
-                })
-            })
-
-        )
-    }, []);
-
-    const handleLeftArrowClick = () => {console.log("left")
-        setOffset((currentOffset) => {
-            const newOffset = currentOffset - 700
-            console.log(newOffset)
-            return newOffset
-        })
-    }
-    const handleRightArrowClick = () => {setOffset((currentOffset) => {
-        const newOffset = currentOffset + 700
-        console.log(newOffset)
-        return newOffset
-    })}
-
     return (
         <div className="App">
-            <div onClick={handleLeftArrowClick}>prev</div>
-            <div className="calendar-wrapper">
+            <div className="wrapper">
                 <CurrentDate classname={styles.CurrentDate} currentDate={currentDate}/>
-                <div className={"window"}>
-                    <Calendar currentYear={currentYear}/>
-                </div>
+                <Calendar currentYear={currentYear} children={children}/>
                 {/*<TaskList/>*/}
             </div>
-            <div onClick={handleRightArrowClick}>next</div>
         </div>
     );
 }
--- src/Components/Calendar/Calendar.jsx
+++ src/Components/Calendar/Calendar.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+import React, {Children, cloneElement, useEffect, useState} from "react";
 import styles from "./Calendar.module.css"
 import Month from "./Month";
 
-const Calendar = ({currentYear}) => {
-    console.log("render")
+const PAGE_WIDTH = 700
+
+const Calendar = ({currentYear, children}) => {
+    // console.log("render")
 
     let january = currentYear.January
     let february = currentYear.February
@@ -18,12 +20,53 @@ const Calendar = ({currentYear}) => {
     let november
     let december
 
+    const [pages, setPages] = useState([])
+    const [offset, setOffset] = useState(0)
+
+    const handleLeftClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset + PAGE_WIDTH
+            console.log(newOffset)
+            return Math.min(newOffset, 0)
+        })
+    }
+    const handleRightClick = () => {
+        setOffset((currentOffset) => {
+            const newOffset = currentOffset - PAGE_WIDTH
+
+            const maxOffset = -(PAGE_WIDTH * (pages.length -1))
+
+            console.log(newOffset)
+            return Math.max(newOffset, maxOffset)
+        })
+    }
+
+    useEffect(() => {
+        setPages(
+            Children.map(children, child => {
+                return cloneElement(child, {
+                    style: {
+                        height: "100%",
+                        minWidth: `${PAGE_WIDTH}px`,
+                        maxWidth: `${PAGE_WIDTH}px`,
+                    }
+                })
+            })
+        )
+    }, [])
+
     return (
-       <div className={styles.Calendar}>
-           <Month month={january}/>
-           <Month month={february}/>
-           <Month month={march}/>
-       </div>
+        <div className={styles.Calendar}>
+            <div className={styles.button} onClick={handleLeftClick}>prev</div>
+                <div className={styles.window}>
+                    <div className={styles.allPagesContainer}>
+                        <Month month={january}/>
+                        <Month month={february}/>
+                        <Month month={march}/>
+                    </div>
+                </div>
+            <div className={styles.button} onClick={handleRightClick}>next</div>
+        </div>
     )
 }
 
